53b7d5ab048015155131c4a95fbe14cce278bc9b
Siaram2002 <siaramayodya@gmail.com>
2025-11-29 21:29:48 +0530

    commit

---
+++ college_management_system/src/main/java/com/cms/transport/enums/BusStatus.java
@@ -0,0 +1,34 @@
+package com.cms.transport.enums;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public enum BusStatus {
+
+    ACTIVE("ACTIVE"),            // Bus is available and running
+    IN_MAINTENANCE("IN_MAINTENANCE"), // Bus is under maintenance
+    OUT_OF_SERVICE("OUT_OF_SERVICE"), // Bus is temporarily not operational
+    ASSIGNED("ASSIGNED"),        // Bus is currently assigned to a route/driver
+    INACTIVE("INACTIVE");        // Bus is decommissioned or retired
+
+    private final String value;
+
+    BusStatus(String value) {
+        this.value = value;
+    }
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
+
+    @JsonCreator
+    public static BusStatus fromValue(String value) {
+        for (BusStatus status : values()) {
+            if (status.value.equalsIgnoreCase(value)) {
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("Unknown BusStatus: " + value);
+    }
+}
